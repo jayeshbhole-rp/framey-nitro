0697b1b720690b7b124d74c349a30dcb377b4601
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-05-08 12:17:28 +0530

    fix: update pf server link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,4 @@
 export const NATIVE = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+
+export const PF_SERVER =
+  process.env.NODE_ENV === 'production' ? 'https://pathfinder-frames.onrender.com' : 'http://localhost:3200';
