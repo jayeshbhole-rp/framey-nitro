e867fbe3e16ac96f910d8cdd79867854a403e794
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-05-09 15:03:02 +0530

    fix: tx route

--- src/app/frames/tx/bridge/route.tsx
+++ src/app/frames/tx/bridge/route.tsx
@@ -1,25 +1,33 @@
 /* eslint-disable react/jsx-key */
 
 import { getTransactionById } from '@/utils/pathfinder';
-import { TransactionTargetResponse } from 'frames.js';
-import { NextResponse } from 'next/server';
+import { getFrameMessage, TransactionTargetResponse } from 'frames.js';
+import { NextRequest, NextResponse } from 'next/server';
 import { frames } from '../../frames';
 import { DexSpanAbi } from '@/constants/abi/DexSpan';
 import { AssetForwarderAbi } from '@/constants/abi/AssetForwarder';
 
-export const POST = frames(async (ctx) => {
-  if (!ctx.message || !ctx.message?.connectedAddress) {
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const json = await req.json();
+  const searchParams = new URLSearchParams(req.url.split('?')[1]);
+  const sessionKey = searchParams.get('sessionKey');
+
+  const frameMessage = await getFrameMessage(json);
+
+  if (!sessionKey) {
+    throw new Error('No session key');
+  }
+  if (!frameMessage || !frameMessage?.connectedAddress) {
     throw new Error('No connected address');
   }
 
-  console.log(ctx.message.connectedAddress);
+  console.log(frameMessage.connectedAddress);
 
   const quote = await getTransactionById({
-    key: ctx.searchParams.sessionKey,
-    sender: ctx.message.connectedAddress,
+    key: sessionKey,
+    sender: frameMessage.connectedAddress,
   });
 
-  //   console.log('state', frameMessage.state);
   if (!quote) {
     throw new Error('No quote');
   }
@@ -29,9 +37,9 @@ export const POST = frames(async (ctx) => {
     method: 'eth_sendTransaction',
     params: {
       abi: quote.source.tokenPath ? DexSpanAbi : AssetForwarderAbi,
-      to: quote.txn.to as `0x${string}`,
+      to: quote.txn.to,
       value: quote.txn.value,
       data: quote.txn.data,
     },
   } as TransactionTargetResponse);
-});
+};
