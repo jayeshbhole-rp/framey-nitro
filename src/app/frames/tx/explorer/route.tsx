88f5a3130e8c085e970060666b29a7a788bac977
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-05-23 16:10:49 +0530

    feat: added explorer

--- src/app/frames/tx/explorer/route.tsx
+++ src/app/frames/tx/explorer/route.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-key */
 
 import { frames } from '@/app/frames/frames';
+import { tokenWhitelist } from '@/constants';
+import { ChainIds, CHAINS } from '@/constants/wagmiConfig';
+import { capitalize, getImageURI } from '@/utils';
+import { formatNumber } from '@/utils/formatNumber';
+import { getTransactionFromExplorer } from '@/utils/getTransactionFromExplorer';
 import { Button } from 'frames.js/next';
+import { formatUnits } from 'viem';
 
 export const runtime = 'edge';
 const joystixFont = fetch(new URL('/public/fonts/joystix_monospace.ttf', import.meta.url)).then((res) =>
@@ -19,25 +26,146 @@ const handleRequest = frames(async (ctx) => {
   if (!ctx.message?.transactionId) {
     throw new Error('No transaction ID in message');
   }
-  currentState.tx = ctx.message?.transactionId;
+  currentState.tx = ctx.message.transactionId;
 
-  return {
-    image: (
-      <div tw='flex h-full w-full flex-col gap-2 bg-neutral-900 text-neutral-100 items-center justify-center'>
-        <span tw='text-[4rem] text-green-500'>Transaction Successful</span>
+  const tx = await getTransactionFromExplorer(currentState.tx);
 
-        <span>Track your transaction on the explorer</span>
-      </div>
-    ),
-    buttons: [
-      <Button action='post'>Check Status</Button>,
+  if (!tx) {
+    throw new Error('No transaction found');
+  }
+
+  let buttons: any = [];
+  if (tx.status === 'completed' || tx.status === 'failed') {
+    buttons = [
+      <Button
+        action='post'
+        target='/'
+      >
+        Bridge again
+      </Button>,
       <Button
         action='link'
         target={`https://explorer.routernitro.com/tx/${ctx.message?.transactionId}`}
       >
         Explorer
       </Button>,
-    ],
+    ];
+  } else {
+    buttons = [
+      <Button
+        action='post'
+        target='/tx/explorer'
+      >
+        Refresh Status
+      </Button>,
+      <Button
+        action='link'
+        target={`https://explorer.routernitro.com/tx/${ctx.message?.transactionId}`}
+      >
+        Explorer
+      </Button>,
+    ];
+  }
+
+  return {
+    image: (
+      <div tw='flex h-full w-full flex-col bg-[#fff] text-neutral-100 items-center p-8'>
+        {/* bg */}
+        <img
+          tw='absolute top-0 left-0 w-[916px] h-[480px]'
+          src={getImageURI('/images/template.png')}
+          alt=''
+          width={916}
+          height={480}
+        />
+
+        <div tw='mt-10' />
+
+        <div
+          tw='flex w-full flex-col items-center'
+          style={{
+            fontFamily: 'Joystix',
+          }}
+        >
+          {!tx?.src_tx_hash ? (
+            <div
+              tw='flex w-full flex-col items-center'
+              style={{
+                fontFamily: 'Joystix',
+              }}
+            >
+              <span tw='text-[2rem] text-green-500'>
+                Submitted on {capitalize(CHAINS[currentState.p.fromChainId as ChainIds])}
+              </span>
+            </div>
+          ) : !tx?.dest_tx_hash ? (
+            <div
+              tw='flex w-full flex-col items-center'
+              style={{
+                fontFamily: 'Joystix',
+              }}
+            >
+              <span tw='text-[2rem] text-yellow-500'>
+                Zapping to {capitalize(CHAINS[currentState.p.toChainId as ChainIds])}
+              </span>
+            </div>
+          ) : tx.status === 'completed' ? (
+            <div
+              tw='flex flex-col items-start'
+              style={{
+                fontFamily: 'Joystix',
+              }}
+            >
+              <span tw='mx-auto'>Zapped</span>
+
+              <span tw='text-red-500'>
+                <img
+                  tw='w-[2.5rem] h-[2.5rem] mr-4'
+                  src={getImageURI(`/images/chains/${CHAINS[Number(tx.src_chain_id) as ChainIds]}.png`)}
+                  width={64}
+                  height={72}
+                  alt=''
+                />
+                {formatNumber(tx.src_amount)} {tx?.src_symbol}
+              </span>
+
+              <img
+                tw='w-[2rem] h-[3rem] mx-auto'
+                src={getImageURI('/images/thunder.png')}
+                width={32}
+                height={32}
+                alt=''
+              />
+
+              <span tw='text-green-500'>
+                <img
+                  tw='w-[2.5rem] h-[2.5rem] mr-4'
+                  src={getImageURI(`/images/chains/${CHAINS[Number(tx.dest_chain_id) as ChainIds]}.png`)}
+                  width={64}
+                  height={64}
+                  alt=''
+                />
+                {formatNumber(tx.dest_amount)} {tx?.dest_symbol}
+              </span>
+            </div>
+          ) : (
+            <div
+              tw='flex w-full flex-col items-center'
+              style={{
+                fontFamily: 'Joystix',
+              }}
+            >
+              <span tw='text-[2rem] text-red-500'>Zap Failed</span>
+            </div>
+          )}
+        </div>
+
+        <span tw='absolute bottom-4 left-0 w-[916px] flex justify-center text-center'>
+          Track your transaction on the explorer
+        </span>
+      </div>
+    ),
+    buttons,
     state: currentState,
     imageOptions: {
       width: 916,
