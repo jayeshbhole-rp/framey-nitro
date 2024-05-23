4aaac3713efea4c44b6b2a1aa3c6af2e686e6c99
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-05-23 15:20:18 +0530

    chore: add decimals to tokenlist

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,6 +11,7 @@ export type TokenData = {
   address: string;
   symbol: string;
   chainId: number;
+  decimals: number;
 };
 export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
   8453: {
@@ -18,11 +19,13 @@ export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
       address: NATIVE,
       symbol: 'ETH',
       chainId: 8453,
+      decimals: 18,
     },
     '0x4ed4e862860bed51a9570b96d89af5e1b0efefed': {
       address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
       symbol: 'DEGEN',
       chainId: 8453,
+      decimals: 18,
     },
   },
   1: {
@@ -30,6 +33,7 @@ export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
       address: NATIVE,
       symbol: 'ETH',
       chainId: 1,
+      decimals: 18,
     },
   },
   10: {
@@ -37,6 +41,7 @@ export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
       address: NATIVE,
       symbol: 'ETH',
       chainId: 10,
+      decimals: 18,
     },
   },
   42161: {
@@ -44,6 +49,7 @@ export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
       address: NATIVE,
       symbol: 'ETH',
       chainId: 42161,
+      decimals: 18,
     },
   },
 };
