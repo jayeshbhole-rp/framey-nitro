0c2d993647961ab08768516491f23d5fda3f7bb0
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-05 11:03:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ const config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        joystix: ['Joystix', 'monospace'],
+      },
     },
   },
   plugins: [],
