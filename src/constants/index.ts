536fbf1e4235fad66dc0961588a0b210a1a6bce0
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-20 19:27:01 +0530

    chore: whitelist tokens

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import { CHAIN_IDS } from './chains';
+
 export const NATIVE = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 
 export const PF_SERVER =
@@ -48,6 +50,103 @@ export const tokenWhitelist: Record<string, Record<string, TokenData>> = {
       symbol: 'ETH',
       chainId: 42161,
       decimals: 18,
+    },
+  },
+
+  [CHAIN_IDS.polygon]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'MATIC',
+      chainId: CHAIN_IDS.polygon,
+      decimals: 18,
+    },
+  },
+  [CHAIN_IDS.zksync]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      chainId: CHAIN_IDS.zksync,
+      decimals: 18,
+    },
+  },
+  [CHAIN_IDS.avalanche]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'AVAX',
+      decimals: 18,
+      chainId: CHAIN_IDS.avalanche,
+    },
+  },
+  [CHAIN_IDS.binance]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.binance,
+    },
+  },
+  [CHAIN_IDS.polygonzkevm]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.polygonzkevm,
+    },
+  },
+  [CHAIN_IDS.manta]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.manta,
+    },
+  },
+  [CHAIN_IDS.mantle]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.mantle,
+    },
+  },
+  [CHAIN_IDS.linea]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.linea,
+    },
+  },
+  [CHAIN_IDS.blast]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.blast,
+    },
+  },
+  [CHAIN_IDS.metis]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.metis,
+    },
+  },
+  [CHAIN_IDS.mode]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.mode,
+    },
+  },
+  [CHAIN_IDS.boba]: {
+    [NATIVE]: {
+      address: NATIVE,
+      symbol: 'ETH',
+      decimals: 18,
+      chainId: CHAIN_IDS.boba,
     },
   },
 };
